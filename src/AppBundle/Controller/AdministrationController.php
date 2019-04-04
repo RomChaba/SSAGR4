eb6c30d15ef2c8af7d4fe64fde9e3f14ef412858
gael <gaelvec@gmail.com>
2019-04-04 15:35:23 +0200

    Début administration

---
+++ src/AppBundle/Controller/AdministrationController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gvecoven
+ * Date: 04/04/2019
+ * Time: 15:09
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdministrationController extends Controller
+{
+    /**
+     * @Route("/administration", name="administration")
+     */
+    public function administrationAction(Request $request)
+    {
+        $personnnes =  Array();
+        $voitures =  Array();
+
+        return $this->render('Administration/administration.html.twig', array('personnes' => $personnnes, 'voitures' => $voitures));
+    }
+
+    /**
+     * @Route("/creationVoiture", name="creationVoiture")
+     */
+    public function creationVoitureAction(Request $request)
+    {
+        $personnnes =  Array();
+        $voitures =  Array();
+
+        return $this->render('Administration/administration.html.twig', array('personnes' => $personnnes, 'voitures' => $voitures));
+    }
+
+    /**
+     * @Route("/creationPersonne", name="creationPersonne")
+     */
+    public function creationPersonneAction(Request $request)
+    {
+        $personnnes =  Array();
+        $voitures =  Array();
+
+        return $this->render('Administration/administration.html.twig', array('personnes' => $personnnes, 'voitures' => $voitures));
+    }
+}
