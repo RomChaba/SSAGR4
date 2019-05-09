0744999570ec1cb676f1c92047ef935653499a33
Samantha <giraudeau.samantha@gmail.com>
2019-05-09 14:16:38 +0200

    charte conducteurs
    Signed-off-by: Samantha <giraudeau.samantha@gmail.com>

---
+++ src/AppBundle/Controller/CharteConducteurController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+//ob_start();
+//session_start();
+use AppBundle\Entity\Emprunt;
+use AppBundle\Entity\Personne;
+use AppBundle\Entity\Site;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use AppBundle\Entity\Vehicule;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class CharteConducteurController extends Controller
+{
+    /**
+     * @Route("/charteconducteur", name="charteconducteur")
+     */
+    public function CharteConducteurAction()
+    {
+        return $this->render('charteconducteur.html.twig');
+
+    }
+}
